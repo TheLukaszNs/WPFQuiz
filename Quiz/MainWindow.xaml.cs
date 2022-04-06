42c83e8d993070542b563ce7f3c2fb0c4962fc5a
unknown <grabiec.kacper01@gmail.com>
2022-04-06 12:23:10 +0200

    Link projects

--- Quiz/MainWindow.xaml.cs
+++ Quiz/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using CommonLibrary;
 
 namespace QuizSolver
 {
